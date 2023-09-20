0f74fa3cb528e47f98731ee0ebd6b46c556ca1b3
Devin Lane <devin.c.lane@gmail.com>
2023-09-19 21:05:45 -0700

    stateful buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex w-full flex-col items-center justify-center space-y-4">
-        <h1>Hello world</h1>
-        <MyButton />
-        <MyButton />
-      </div>
-    </main>
-  )
+      <main className="min-h-screen flex flex-col items-center bg-gray-100 py-10 px-4 sm:px-6 lg:px-8 gap-4">
+          <h1>Button with independent states</h1>
+          <Button />
+          <Button />
+      </main>
+  );
 }
 
-function MyButton() {
-  return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Button
-    </button>
-  )
+function Button() {
+  const [count, setCount] = useState(0);
 
+  function handleClick() {
+      setCount(count + 1);
+  }
+
+  return (
+      <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button clicked {count} times
+      </button>
+  );
 }
